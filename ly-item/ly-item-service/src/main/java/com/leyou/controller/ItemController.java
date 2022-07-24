ebad04dc096aae94fb68eb37ec991aea54882c77
orgin <jeneboss@163.com>
2022-07-24 19:41:07 +0800

    通用异常1场景预设+log配置

--- ly-item/ly-item-service/src/main/java/com/leyou/controller/ItemController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/controller/ItemController.java
@@ -5,15 +5,16 @@ import com.leyou.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
     @Autowired
     private ItemService itemService;
-
+    @PostMapping("item")
     public ResponseEntity<Item> saveItem(Item item){
-        if(item.getPrice()==0){
+        if(item.getPrice()== null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
         Item result = itemService.saveItem(item);
--- ly-item/ly-item-service/src/main/java/com/leyou/service/impl/ItemServiceImpl.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/service/impl/ItemServiceImpl.java
@@ -2,17 +2,20 @@ package com.leyou.service.impl;
 
 import com.leyou.pojo.Item;
 import com.leyou.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
 public class ItemServiceImpl implements ItemService {
-
+    private Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
     @Override
     public Item saveItem(Item item) {
         int id = new Random().nextInt(100);
         item.setId(id);
+        logger.info("查看item",item.toString());
         return item;
     }
 }
